7f7296eec34d46c4d6f03faf6abdfa8b9ff48573
lrodriguez <lrodriguez@PEE5B42>
2024-11-11 17:44:14 -0500

    base datos

--- listas.py
+++ listas.py
@@ -1,21 +1,4 @@
-def fibonacci(limit):
-    a, b = 0, 1
-    while a < limit:
-        yield a
-        a, b = b, a+b
+import tkinter
 
-
-def numerospares(limit):
-    a = 0
-    while a <= limit:
-        if a % 2 == 0:
-            yield a
-        a += 1
-
-
-for num in numerospares(10):
-    print(num)
-
-# for num in fibonacci(10):
-#    print(num)
-
+root= tkinter.Tk()
+root.mainloop()
